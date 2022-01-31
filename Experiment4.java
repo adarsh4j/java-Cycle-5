8636ecfbb35b0c13a29bf474ca4f8a4c9c80f9e6
Adarsh.J <78248655+Adarsh-pgmr@users.noreply.github.com>
2022-01-31 16:37:30 +0530

    Add files via upload

---
+++ Experiment4.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class Experiment4
+{
+	public static void main(String args[])throws Exception
+	{
+		FileInputStream fin=new FileInputStream("int1.txt");
+		String a;
+		int i,s=0;
+		while((i=fin.read())!=-1)
+		{
+			System.out.print((char)i);
+			a=Character.toString((char)i);
+			StringTokenizer st=new StringTokenizer(a);
+			while(st.hasMoreTokens())
+			{
+				a=st.nextToken();
+				int b=Integer.parseInt(a);
+				s+=b;
+			}
+		}
+		System.out.format("The sum of numbers in file is:%d",s);
+		
+	}
+}
